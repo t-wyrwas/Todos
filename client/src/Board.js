1fe15f17be0a25947c57e2f2ccef679ab6ff153a
twyrwas <tomasz.wyrwas@gmail.com>
2019-03-24 19:51:23 +0000

    Add html to present 3 columns of tasks: todo, doing, done

--- client/src/Board.js
+++ client/src/Board.js
@@ -5,15 +5,90 @@ class Board extends Component {
 
     constructor() {
         super();
+        this.state = {
+            todo: [
+                {
+                    name: "do shoping",
+                    details: "buy some bread"
+                },
+                {
+                    name: "go to school",
+                    details: "learn something finally"
+                }
+            ],
+            doing: [
+                {
+                    name: "read a book",
+                    details: "be smarter"
+                },
+            ],
+            done: [
+                {
+                    name: "by a car",
+                    details: "hyundai i30"
+                },
+                {
+                    name: "do something with your life",
+                    details: "e.g. stop living in a cellar"
+                },
+            ],
+        }
     }
-    
+
     componentWillMount() {
     }
 
     render() {
         return (
             <div className="board">
-                <span>Project: {this.props.projectId}</span>
+                <h2>Project: {this.props.projectId}</h2>
+                <div className="board-task-area">
+                    <div className="status-column">
+                        <div>
+                            <h3>To do</h3>
+                        </div>
+                        {this.state.todo.map(function(task) {
+                            return <div className="task">
+                                    <div className="task-name">
+                                        {task.name}
+                                    </div>
+                                    <div className="task-details">
+                                        {task.details}
+                                    </div>
+                                </div>
+                        })}
+                    </div>
+                    <div className="status-column">
+                        <div>
+                            <h3>Doing</h3>
+                        </div>
+                        {this.state.doing.map(function(task) {
+                            return <div className="task">
+                                    <div className="task-name">
+                                        {task.name}
+                                    </div>
+                                    <div className="task-details">
+                                        {task.details}
+                                    </div>
+                                </div>
+                        })}
+                    </div>
+                    <div className="status-column">
+                        <div>
+                            <h3>Done</h3>
+                        </div>
+                        {this.state.done.map(function(task) {
+                            return <div className="task">
+                                    <div className="task-name">
+                                        {task.name}
+                                    </div>
+                                    <div className="task-details">
+                                        {task.details}
+                                    </div>
+                                </div>
+                        })}
+                    </div>
+                </div>
             </div>
         );
     }
